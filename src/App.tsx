25e10dec1ac61ffa6b92ec04108dea0a2d4ddc41
Maximiliano Friedl <mnfriedl@uc.cl>
2024-10-15 13:24:18 -0300

    feat(layout): basic layout components and structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import './App.css'
+import CityTabs from './components/layout/CityTabs'
+import Navbar from './components/layout/Navbar'
+import WeatherPage from './pages/weather'
+
+//? We could add react-router here if we needed more pages, but we don't, so keep it simple.
 
 function App() {
   return (
-    <div>hola</div>
+    <div className="flex flex-col bg-blue-300 min-h-[100vh]">
+      <Navbar />
+      <CityTabs />
+      <WeatherPage />
+    </div>
   )
 }
 

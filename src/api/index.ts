84f108e81eeb640ab5c4857670d15768d098eae5
Maximiliano Friedl <mnfriedl@uc.cl>
2024-10-15 13:30:42 -0300

    feat(axios): axios instance

---
+++ src/api/index.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+import { apiKey } from "@/consts/weatherApi";
+
+const api = axios.create({
+  baseURL: "api.openweathermap.org/data/2.5",
+  params: {
+    appid: apiKey,
+  }
+});
+
+export default api;
---
+++ src/consts/weatherApi.ts
@@ -0,0 +1,2 @@
+//! This should be in an env var
+export const apiKey = "482944e26d320a80bd5e4f23b3de7d1f";
